7bb32cb1079da73c57b80c4151e5a5d68cb95557
Sylvia Alice <Sylviaalice58@gmail.com>
2026-03-12 14:06:09 +0300

    update

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
@@ -8,13 +9,68 @@ const Signup = () => {
   const[password,setPassword]=useState("");
   const[phone,setPhone]=useState("");
 
+  // Define the three states an application will move to
+  const[loading,setLoading]=useState("");
+  const[success,setSuccess]=useState("");
+  const[error,setError]=useState("");
+
+  // Below is a function that will handle the submit action
+  const handleSubmit = async(e) =>{
+    // Below we prevent our site from reloading
+    e.preventDefault()
+
+    // Update our loading hook with a message that will be displayed to the users who are trying to register
+    setLoading("Please wait as registration is in progress...")
+
+    try{
+      // Create a form data object that will enable you to cupture the four details entered on the form
+      const formdata = new FormData();
+
+      // insert the four detals(username, email,password,phone) interms of key-value pairs
+      formdata.append("username",username);
+      formdata.append("email",email);
+      formdata.append("password",password);
+      formdata.append("phone",phone);
+
+      // by use o axios we can access the method post
+      const response =await axios.post("https://slyney2248.alwaysdata.net/api/signup",formdata)
+
+      // set back the loading hook to default
+      setLoading("");
+
+      // Just in case everything goes on well update the success hook with a message
+      setSuccess(response.data.message)
+      
+
+      // clear your hooks
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setPhone("");
+    }
+    catch(error){
+      // set the loading hook back to default
+      setLoading("");
+
+      // Update the error hook with the message given from response
+      setError(error.message)
+
+    }
+
+  }
+
 
   return (
     <div className='row justify-content-center mt-4'>
         <div className="card col-md-6 shadow p-4">
           <h1 className='text-primary'>Sign Up</h1>
 
-          <form >
+          <h5 className="text-warning">{loading}</h5>
+          <h3 className="text-success">{success}</h3>
+          <h4 className="text-center">{error}</h4>
+
+
+          <form onSubmit={handleSubmit}>
             <input type="text"  
             placeholder='Enter the username'
             className='form-control'
@@ -51,7 +107,7 @@ const Signup = () => {
 
             {/* {phone} */}
 
-            <input type="button" value="Signup" className='btn btn-primary'/> <br /> <br />
+            <input type="submit" value="Signup" className='btn btn-primary'/> <br /> <br />
 
             Already have an account? <Link to={'/signin'}>Signin</Link>
 
